40a5c898436a51549e9dcd60b288d36234a33659
malloryrbush <malloryrbush@hotmail.com>
2018-08-18 19:33:21 -0400

    first commit

---
+++ PetFinderPlus/Models/AnimalType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PetFinderPlus.Models
+{
+    public class AnimalType
+    {
+        [Key]
+        public int AnimalTypeID { get; set; }
+        public string Dog { get; set; }
+        public string Cat { get; set; }
+        public string Snake { get; set; }
+    }
+}
---
+++ PetFinderPlus/Models/Breed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PetFinderPlus.Models
+{
+    public class Breed
+    {
+        [Key]
+        public int BreedId { get; set; }
+        public string PitBull { get; set; }
+        public string GermanShephard { get; set; }
+        public string Siamese { get; set; }
+        public string BlackCat { get; set; }
+        public string BlackMamba { get; set; }
+        public string Python { get; set; }
+
+        [ForeignKey("AnimalTypeId")]
+        public int AnimalTypeId { get; set; }
+        public virtual AnimalType AnimalType { get; set; }
+    }
+}
